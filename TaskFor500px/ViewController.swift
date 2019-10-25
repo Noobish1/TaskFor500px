9b072ac401f2aac34150b7e930f01ed44f0664f5
Blair McArthur <blair.mcarthur@icloud.com>
2019-10-25 13:07:41 -0400

    Initial Commit

---
+++ TaskFor500px/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskFor500px
+//
+//  Created by Blair McArthur on 2019-10-25.
+//  Copyright © 2019 Noobish1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
