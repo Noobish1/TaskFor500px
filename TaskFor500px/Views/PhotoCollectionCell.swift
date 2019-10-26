cc1fad0ded5cf3e9a968c93c395cae4456c4644f
Blair McArthur <blair.mcarthur@icloud.com>
2019-10-25 20:59:53 -0400

    Added a basic UICollectionViewCell subclass for testing collectionview layouts

---
+++ TaskFor500px/Views/PhotoCollectionCell.swift
@@ -0,0 +1,33 @@
+import UIKit
+import Then
+
+internal final class PhotoCollectionCell: UICollectionViewCell {
+    // MARK: properties
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    // MARK: init
+    internal override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        contentView.backgroundColor = UIColor(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1
+        )
+        
+        setupViews()
+    }
+    
+    @available(*, unavailable)
+    internal required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setup
+    private func setupViews() {
+        contentView.add(fullscreenSubview: imageView)
+    }
+}
