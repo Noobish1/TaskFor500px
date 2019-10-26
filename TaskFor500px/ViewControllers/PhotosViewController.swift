99ceb26a0b3473c4f3c5f425d80abbabfa6c8a60
Blair McArthur <blair.mcarthur@icloud.com>
2019-10-25 23:30:31 -0400

    Added image downloading/caching to the PhotosViewController.

--- TaskFor500px/ViewControllers/PhotosViewController.swift
+++ TaskFor500px/ViewControllers/PhotosViewController.swift
@@ -1,4 +1,5 @@
 import GreedoLayout
+import Kingfisher
 import RxSwift
 import SnapKit
 import UIKit
@@ -14,16 +15,26 @@ internal final class PhotosViewController: UIViewController {
             $0.sectionInset = UIEdgeInsets(top: 10, left: 5, bottom: 5, right: 5)
         }
     ).then {
+        $0.backgroundColor = .white
         $0.dataSource = self
         $0.delegate = self
+        $0.prefetchDataSource = self
         $0.register(PhotoCollectionCell.self, forCellWithReuseIdentifier: cellIdentifier)
     }
     private lazy var greedoLayout = GreedoCollectionViewLayout(collectionView: collectionView).then {
+        $0.fixedHeight = false
+        $0.rowMaximumHeight = collectionView.bounds.height / 3
         $0.dataSource = self
     }
     private let disposeBag = DisposeBag()
     private let photosClient = PhotosClient()
+    private let imageDownloader = ImageDownloader.default
     private var response: PopularPhotosResponse?
+    
+    // MARK: status bar
+    internal override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
     
     // MARK: init
     @available(*, unavailable)
@@ -42,7 +53,7 @@ internal final class PhotosViewController: UIViewController {
         view.backgroundColor = .white
         
         view.add(subview: collectionView, withConstraints: { make in
-            make.top.equalToSuperview()
+            make.top.equalToSuperviewOrSafeAreaLayoutGuide()
             make.leading.equalToSuperviewOrSafeAreaLayoutGuide()
             make.trailing.equalToSuperviewOrSafeAreaLayoutGuide()
             make.bottom.equalToSuperview()
@@ -79,6 +90,29 @@ internal final class PhotosViewController: UIViewController {
     }
 }
 
+// MARK: UICollectionViewDataSourcePrefetching
+extension PhotosViewController: UICollectionViewDataSourcePrefetching {
+    private func imageURL(for indexPath: IndexPath) -> URL? {
+        return response?.photos[indexPath.item].images.first(where: { $0.size == ImageSize.grid.rawValue })?.url
+    }
+    
+    private func validImageURLs(for indexPaths: [IndexPath]) -> [URL] {
+        return indexPaths.compactMap { imageURL(for: $0) }
+    }
+    
+    internal func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        validImageURLs(for: indexPaths).forEach { url in
+            imageDownloader.downloadImage(with: url)
+        }
+    }
+
+    internal func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        validImageURLs(for: indexPaths).forEach { url in
+            imageDownloader.cancel(url: url)
+        }
+    }
+}
+
 // MARK: UICollectionViewFlowLayoutDataSource
 extension PhotosViewController: UICollectionViewDataSource {
     internal func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -87,6 +121,14 @@ extension PhotosViewController: UICollectionViewDataSource {
     
     internal func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: PhotoCollectionCell = collectionView.n1_dequeueReusableCell(identifier: cellIdentifier, indexPath: indexPath)
+        
+        if let url = imageURL(for: indexPath) {
+            if let image: UIImage = ImageCache.default.retrieveImageInMemoryCache(forKey: url.absoluteString) {
+                cell.imageView.image = image
+            } else {
+                cell.imageView.kf.setImage(with: url)
+            }
+        }
         
         return cell
     }
