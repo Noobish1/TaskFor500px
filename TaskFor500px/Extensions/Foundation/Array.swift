38397d8c012152aa766fe7afdbd58551487a82fc
Blair McArthur <blair.mcarthur@icloud.com>
2019-10-27 17:25:55 -0400

    added some more Array extensions

--- TaskFor500px/Extensions/Foundation/Array.swift
+++ TaskFor500px/Extensions/Foundation/Array.swift
@@ -2,8 +2,40 @@ import Foundation
 
 // MARK: General extensions
 extension Array {
+    // MARK: appending
+    internal func byAppending(_ element: Element) -> [Element] {
+        var array = self
+        array.append(element)
+
+        return array
+    }
+
+    internal func byAppending(_ elements: [Element]) -> [Element] {
+        var array = self
+        array.append(contentsOf: elements)
+
+        return array
+    }
+    
     // MARK: safe subscripting
     internal subscript (safe index: Int) -> Element? {
         return index < count ? self[index] : nil
+    }
+}
+
+// MARK: Equatable extensions
+extension Array where Element: Equatable {
+    // MARK: replacing
+    internal func byReplacing(_ element: Element, with otherElement: Element) -> [Element] {
+        var array = self
+        array.replace(element, with: otherElement)
+
+        return array
+    }
+
+    internal mutating func replace(_ element: Element, with otherElement: Element) {
+        guard let index = firstIndex(of: element) else { return }
+
+        self[index] = otherElement
     }
 }
