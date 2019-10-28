bca784eb6322e94d5b7268d790a0e19bf16f53b8
Blair McArthur <blair.mcarthur@icloud.com>
2019-10-28 01:58:40 -0400

    added a NavStackEmbedded protocol

---
+++ TaskFor500px/Protocols/NavStackEmbedded.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+public protocol NavStackEmbedded: UIViewController {
+    var navController: UINavigationController { get }
+}
+
+extension NavStackEmbedded {
+    // MARK: computed properties
+    public var navController: UINavigationController {
+        guard let navController = self.navigationController else {
+            fatalError("\(self) should be embedded in a UINavigationController but is not")
+        }
+
+        return navController
+    }
+}
