5cc636c9dc8545e55145b69ee62b99fe7bd3628a
Blair McArthur <blair.mcarthur@icloud.com>
2019-10-28 01:56:30 -0400

    tweaked the Kingfisher ImageCache settings to reduce memory usage

--- TaskFor500px/Application/AppDelegate.swift
+++ TaskFor500px/Application/AppDelegate.swift
@@ -1,3 +1,4 @@
+import Kingfisher
 import Then
 import UIKit
 
@@ -6,6 +7,20 @@ import UIKit
 internal final class AppDelegate: UIResponder {
     // MARK: properties
     internal var window: UIWindow?
+    
+    // MARK: setup
+    private func setupImageCache() {
+        let oneHundredMB = 100 * 1024 * 1024
+        
+        let costLimit = ProcessInfo.processInfo.physicalMemory / 4
+        let memoryStorage = (costLimit > Int.max) ? Int.max : Int(costLimit)
+        let actualMemoryStorage = min(oneHundredMB, memoryStorage)
+        
+        var config = MemoryStorage.Config(totalCostLimit: actualMemoryStorage, cleanInterval: 30)
+        config.countLimit = 50
+        
+        ImageCache.default.memoryStorage.config = config
+    }
 }
 
 // MARK: UIApplicationDelegate
@@ -16,6 +31,8 @@ extension AppDelegate: UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
         // swiftlint:enable discouraged_optional_collection
     ) -> Bool {
+        setupImageCache()
+        
         self.window = UIWindow(frame: UIScreen.main.bounds).then {
             $0.rootViewController = PhotosContainerViewController()
             $0.makeKeyAndVisible()
