8e9823c196214d6e410772c23652386bea2d6265
Blair McArthur <blair.mcarthur@icloud.com>
2019-10-30 01:12:03 -0400

    Added a comment to DateFormatters.

--- TaskFor500px/Objects/DateFormatters.swift
+++ TaskFor500px/Objects/DateFormatters.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+// I use this to cache dateformatters because they are expensive to create
+// I use an enum because it cannot be initialised
 internal enum DateFormatters {
     internal static let iso8601 = ISO8601DateFormatter().then {
         $0.formatOptions = [.withInternetDateTime]
